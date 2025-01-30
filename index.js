4b466f0618cc83e36b76d4d4818a8fd56d2eb665
Jesus Virgen <jesus.virgencer@gmail.com>
2025-01-30 00:41:08 -0600

    Add views and connect database

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 import express from 'express';
 import router from './routes/index.js';
+import db from './config/db.js';
 
 const app = express();
+
+//conect db
+db.authenticate()
+    .then( () => console.log('Base de datos conectada') )
+    .catch( error => console.log(error));
 
 //defie port
 const port = process.env.PORT || 4000;
 
 //hability PUG
 app.set('view engine', 'pug');
+
+app.use( (req, res, next) => {
+    const year = new Date();
+    res.locals.actualYear = year.getFullYear();
+    res.locals.namePage = 'Agencia de viajes';
+    next();
+});
+
+app.use(express.static('public'));
 
 //add router
 app.use('/', router);
--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,27 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', (req, res)  => {
-    res.render('index');
-})
+    res.render('index', {
+        page : 'Inicio'
+    });
+});
 
 router.get('/about', (req, res)  => {
-    res.render('about');
-})
+    res.render('about', {
+        page : 'Nosotros'
+    });
+});
+
+router.get('/trips', (req, res)  => {
+    res.render('trips', {
+        page : 'Viajes'
+    });
+});
+
+router.get('/testimonials', (req, res)  => {
+    res.render('testimonials', {
+        page : 'Testimonios'
+    });
+});
 
 export default router;
